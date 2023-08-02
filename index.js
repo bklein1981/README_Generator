9e105b3473d467ceaf75798fbcf5da2c08fd743b
Brian Klein <bklein1981@gmail.com>
2023-08-02 16:23:02 -0500

    replaced v9 of inquirer with v8.2.6

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-// TODO: Include packages needed for this application
-const inquirer = require('inquirer');
+//Included Packages
+const generateMarkdown = require("./utils/generateMarkdown");
 const fs = require('fs');
+const inquirer = require("inquirer");
+
 
 // TODO: Create an array of questions for user input
 const questions = [];
